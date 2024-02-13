1d279195807a9ecc22c6a087be7ad58054cfc065
aditi55pathak@gmail.com <aditipathakp@gmailcom>
2024-02-13 23:12:13 +0530

    methods

---
+++ methods.py
@@ -0,0 +1,18 @@
+class Icecream:
+
+    utensil = "Scoop"  # class attribute
+
+    def __init__(self, flavor, price):
+        self.flavor = flavor  # object attribute
+        self.price = price
+        print(f"Taking infos about Icecream --- Flavor is :{flavor},Price is :{price}")
+
+    def welcome(self):
+        print("Hello people!")
+
+
+i = Icecream("Vanilla", 90)
+print(i)
+print(i.flavor)
+print(Icecream.utensil)
+i.welcome()
