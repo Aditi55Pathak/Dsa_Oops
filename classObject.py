c18c7c4a2cbb17e74ae7f4493bfe958c2aaee444
aditi55pathak@gmail.com <aditipathakp@gmailcom>
2024-02-13 22:51:04 +0530

    Class and objects

---
+++ classObject.py
@@ -0,0 +1,19 @@
+# Initially we used procedural programming 
+
+# a=10
+# b=90
+
+# sum=a+b
+# print(sum)
+
+# Than concept of Function came which increased reusablity and decreased redudancy.
+
+# Than came concept of objects and classes, bottom up approcah.
+
+# creating a simple class and object modal
+
+class IceCream:
+    flavor="Vanilla"
+
+i=IceCream()
+print(i.flavor)
